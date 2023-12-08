ad340acdca6b0d5371ded4ac25f9f7f7c33c3109
amr-daoud <amr.f.daoud@gmail.com>
2023-12-08 19:26:12 +0300

    Adding some side nav & icons

--- src/app/side-nav/side-nav.const.ts
+++ src/app/side-nav/side-nav.const.ts
@@ -1,6 +1,59 @@
 import {IconNavItem} from 'techteec-lib/components/icon-side-nav'
-import { ADMIN_ICON, COUNTER_ICON, DEVICE_ICON, SUBSET_ICON } from '../common/app-icons.const'
+import { ADD_ICON, ADMIN_ICON, COUNTER_ICON, DASHBOARD_ICON, DEVICE_ICON, HOME_ICON, KPI_ICON, LIST_ICON, REPORT_ICON, SHARE_ICON, SUBSET_ICON } from '../common/app-icons.const'
 export const items: IconNavItem[] = [
+    {
+        title: 'Home',
+        svgIcon: HOME_ICON
+    },
+    {
+        title: 'KPI',
+        svgIcon: KPI_ICON,
+        postition: 'top',
+        children: [
+            {
+                title: 'All KPIS',
+                svgIcon: LIST_ICON
+            },
+            {
+                title: 'Create New',
+                svgIcon: ADD_ICON
+            }
+        ]
+    },
+    {
+        title: 'Reports',
+        svgIcon: REPORT_ICON,
+        postition: 'top',
+        children: [
+            {
+                title: 'All Reports',
+                svgIcon: LIST_ICON
+            },
+            {
+                title: 'Create New',
+                svgIcon: ADD_ICON
+            }
+        ]
+    },
+    {
+        title: 'Dashboards',
+        svgIcon: DASHBOARD_ICON,
+        postition: 'top',
+        children: [
+            {
+                title: 'My Dashboards',
+                svgIcon: LIST_ICON
+            },
+            {
+                title: 'Shared Dashboards',
+                svgIcon: SHARE_ICON
+            },
+            {
+                title: 'Create New',
+                svgIcon: ADD_ICON
+            }
+        ]
+    },
     {
         title: 'Admin',
         svgIcon: ADMIN_ICON,
