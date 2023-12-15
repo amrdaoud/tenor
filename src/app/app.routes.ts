2b771feb120ecd0c153431200c2993783bc52ac7
amr-daoud <amr.f.daoud@gmail.com>
2023-12-15 18:08:34 +0300

    Fixing KPI builder layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ export const routes: Routes = [
         {path: 'devices', loadComponent: () => import('./devices/device-list/device-list.component').then(c => c.DeviceListComponent)},
         {path: 'counters', loadComponent: () => import('./counters/counter-list/counter-list.component').then(c => c.CounterListComponent)},
         {path: 'kpis/list', loadComponent: () => import('./kpis/kpi-list/kpi-list.component').then(c => c.KpiListComponent)},    
-        
         {path: 'kpis/builder', loadComponent: () => import('./kpis/kpi-builder/kpi-builder.component').then(c => c.KpiBuilderComponent)},
     ]}
 ];
