3eb962aa286d3737c8dbad1a75dfd6f037226cbe
amr-daoud <amr.f.daoud@gmail.com>
2024-03-10 15:51:43 +0300

    Updating Environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-apiUrl: 'https://localhost:7162/api/',
-  // apiUrl: 'https://api.tenor.techteec.net/api/',
+  // apiUrl: 'https://localhost:7162/api/',
+  apiUrl: 'https://api.tenor.techteec.net/api/',
   // apiUrl: 'http://',
   production: true,
   test: false,
