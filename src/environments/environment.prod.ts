c5921441a0c02879dcd1a42285decfc91e15e8b7
amr-daoud <amr.f.daoud@gmail.com>
2024-03-13 13:13:08 +0300

    changing styles for building

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   // apiUrl: 'https://localhost:7162/api/',
-  apiUrl: 'https://api.tenor.techteec.net/api/',
-  // apiUrl: 'http://',
+  // apiUrl: 'https://api.tenor.techteec.net/api/',
+  apiUrl: 'http://seserv192:4006/api/',
   production: true,
   test: false,
   baseHref: '/',
